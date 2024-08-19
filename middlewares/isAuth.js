d7db1faded32644fd02ed5b13b4ec8b27eed5ad4
Youssef Fekry <yossifdiaa4@gmail.com>
2024-08-19 17:37:26 +0300

    middleware isAuth to protect route that require authentication

---
+++ middlewares/isAuth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json('Access denied. No token provided.');
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'your_jwt_secret');
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json('Invalid token');
+  }
+};
+
+module.exports = auth;
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
+const isAuth = require('../middlewares/isAuth');
+
 
 router.post('/signIn', userController.signInUser);
 router.post('/signUp', userController.signUpUser);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUser);
-router.put('/:id', userController.updateUser);
+router.put('/:id', isAuth ,   userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
